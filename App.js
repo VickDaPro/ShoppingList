8df6f204b246eee4827eb34056c88dd0f0f37649
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-15 14:59:52 +0530

    added some list items with delete functionality

--- App.js
+++ App.js
@@ -1,13 +1,33 @@
+import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, View, FlatList } from "react-native";
+import uuid from "react-native-uuid";
+import Header from "./assets/components/Header";
+import ListItem from "./assets/components/ListItem";
 
 export default function App() {
+  const [items, setItems] = useState([
+    { id: uuid.v4(), text: "Milk" },
+    { id: uuid.v4(), text: "Eggs" },
+    { id: uuid.v4(), text: "Bread" },
+    { id: uuid.v4(), text: "Juice" },
+  ]);
+
+  const deleteItem = (id) => {
+    setItems((prevItems) => {
+      return prevItems.filter((item) => item.id != id);
+    });
+  };
+
   return (
     <View style={styles.container}>
-      <Text style={{ color: "darkslateblue", fontSize: 15 }}>
-        Open up App.js to start working on your app!
-      </Text>
-      <StatusBar style="auto" />
+      <Header />
+      <FlatList
+        data={items}
+        renderItem={({ item }) => (
+          <ListItem item={item} deleteItem={deleteItem} />
+        )}
+      />
     </View>
   );
 }
@@ -15,8 +35,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    paddingTop: 60,
   },
 });
---
+++ assets/components/Header.js
@@ -0,0 +1,28 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function Header({ title }) {
+  return (
+    <View style={styles.header}>
+      <Text style={styles.text}>{title}</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+Header.defaultProps = {
+  title: "Shopping List",
+};
+
+const styles = StyleSheet.create({
+  header: {
+    height: 60,
+    padding: 15,
+    backgroundColor: "darkslateblue",
+  },
+  text: {
+    color: "#fff",
+    fontSize: 23,
+    textAlign: "center",
+  },
+});
