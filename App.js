ff25fa1d423c21575b7b3663fdd3d87612c4801d
Vikas Jyani <58882569+VickDaPro@users.noreply.github.com>
2022-06-15 15:45:00 +0530

    app is ready with basic functionality

--- App.js
+++ App.js
@@ -1,9 +1,10 @@
 import { useState } from "react";
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View, FlatList } from "react-native";
+import { StyleSheet, Text, View, FlatList, Alert } from "react-native";
 import uuid from "react-native-uuid";
 import Header from "./assets/components/Header";
 import ListItem from "./assets/components/ListItem";
+import AddItem from "./assets/components/AddItem";
 
 export default function App() {
   const [items, setItems] = useState([
@@ -19,9 +20,18 @@ export default function App() {
     });
   };
 
+  const addItem = (text) => {
+    text
+      ? setItems((prevItems) => {
+          return [{ id: uuid.v4(), text }, ...prevItems];
+        })
+      : Alert.alert("Error", "Please enter an item", { text: "OK" });
+  };
+
   return (
     <View style={styles.container}>
       <Header />
+      <AddItem addItem={addItem} />
       <FlatList
         data={items}
         renderItem={({ item }) => (
